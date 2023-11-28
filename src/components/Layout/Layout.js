1514cfecf2fd9f7f03b2f721ba61385e61c4fffc
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-11-29 01:20:43 +0200

    add gh-pages

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,9 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import { LayoutWrapper, SectionWrapper, OutletWrapper } from './Layout.styled';
+import { Outlet } from "react-router-dom";
+import { LayoutWrapper, SectionWrapper, OutletWrapper } from "./Layout.styled";
 
 const Layout = () => {
-  const location = useLocation();
-  const isHomePage = location.pathname === '/';
+  // const location = useLocation();
+  // const isHomePage = location.pathname === '/';
 
   return (
     <LayoutWrapper>
