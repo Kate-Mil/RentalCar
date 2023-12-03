c6e75860ea0338b099a868613860e539424b34eb
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-12-04 00:04:15 +0200

    fix ettemtp 2

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,24 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import { List, Link } from "./AppBar.styled";
 
 export const AppBar = () => {
-  const navigate = useNavigate();
-
-  const handleButtonClick = (path) => {
-    navigate(path);
-    window.location.reload();
-  };
-
   return (
     <>
       <List>
         <Link to="/">Home</Link>
-        <Link to="/catalog" onClick={() => handleButtonClick("/catalog")}>
-          Catalog
-        </Link>
-        <Link to="/favorites" onClick={() => handleButtonClick("/favorites")}>
-          Favorites
-        </Link>
+        <Link to="/catalog">Catalog</Link>
+        <Link to="/favorites">Favorites</Link>
       </List>
     </>
   );
